f3f42ace5cf4b8e420440be7cf5f06c5c3291492
tgreen <travis@travisgreen.net>
2018-02-15 11:46:17 -0700

    update

--- import.py
+++ import.py
@@ -1,23 +1,35 @@
 #!/usr/bin/env python3
 import json
 import requests
+import configparser
+import sys
 from datetime import datetime
 from elasticsearch import Elasticsearch # sudo pip3 install elasticsearch
-debug = True
 
-# download fresh copy:
-r = requests.get('https://rules.emergingthreatspro.com/PUT_OINKCODE_HERE/reputation/domainrepdata.json')
-if debug: print(r.status_code)
-repdata = r.json()
+def get_ET_rep(config_dict):
+    print(config_dict)
+    r = requests.get(config_dict["url"].replace('oinkcode',config_dict["oinkcode"]))
+    if config_dict["verbose"]: print(r.status_code)
+    return r.json() # returns a dict
 
-# or from local file:
-#repdata = json.load(open('./domainrepdata.json'))
+def load_ET_rep(repdata, config_dict):
+    es = Elasticsearch() # defaults to localhost:9200
+    es.indices.create(index=config_dict["es_index"], ignore=400) # ignore 400 (index already exists)
+    for record, repdict in repdata.items():
+        for category, score in repdict.items():
+            if config_dict["verbose"]: print(record + ' ' + category + ':' + score)
+            es.index(index=config_dict["es_index"], doc_type=config_dict["es_doc_type"], body={config_dict["es_rec_type"]: record, "timestamp": datetime.now(), "category": category, "score": score})    
+    
+def main():
+    config = configparser.ConfigParser()
+    config.read('./config.prv')
 
-es = Elasticsearch() # defaults to localhost:9200
-es.indices.create(index='domainrep', ignore=400) # ignore code 400 (index already exists)
+    repdata = get_ET_rep(dict(config["ETPro_domainrep"]))
+    load_ET_rep(repdata, dict(config["ETPro_domainrep"]))
+    
+    repdata = get_ET_rep(dict(config["ETPro_iprep"]))
+    load_ET_rep(repdata, dict(config["ETPro_iprep"]))
 
-for domain, repdict in repdata.items():
-    for category, score in repdict.items():
-        if debug: print(domain + ' ' + category + ':' + score)
-        es.index(index="domainrep", doc_type="repdata", body={"domain": domain, "timestamp": datetime.now(), "category": category, "score": score})
-        
+if __name__ == "__main__":
+    main()
+    
